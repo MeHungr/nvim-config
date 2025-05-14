e6cfa32594c23d0c250b2b2c56180bbdb652b51e
Dylan Weber <dylanweber06@gmail.com>
2025-05-14 18:45:23 -0400

    Added comments to init.lua

--- init.lua
+++ init.lua
@@ -11,9 +11,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Exit keybind
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, desc = "Exit insert mode with <jj>" })
+
+-- Remove highlighting with Escape key
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>", { noremap = true, silent = true, desc = "Clears highlighting then presses <Esc>" })
 
+-- WhichKey color configuration
 vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "#1e1e2e", fg = "#cdd6f4" })
 vim.api.nvim_set_hl(0, "WhichKeyBorder", { bg = "#1e1e2e", fg = "#89b4fa" })
 vim.api.nvim_set_hl(0, "WhichKeyTitle", { fg = "#f38ba8", bold = true })
