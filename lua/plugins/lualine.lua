d536ff31b0e90504b5417e3243ce171c453626c3
Dylan Weber <dylanweber06@gmail.com>
2025-05-25 18:43:03 -0400

    Global status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,14 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'nightfly'
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "nightfly",
+				icons_enabled = true,
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				globalstatus = true,
+			}
+		})
+	end,
 }
